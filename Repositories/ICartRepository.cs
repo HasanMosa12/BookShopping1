b68db1c5cba90b106c961f38824ca1392ef50d02
Ali Heib <129912849+Alihaib@users.noreply.github.com>
2024-12-20 16:15:40 +0200

    Delete Repositories directory

--- Repositories/ICartRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BookShopping1.Repositories
-{
-    public interface ICartRepository
-    {
-         Task<bool> AddItem(int bookId, int qty);
-         Task<bool> RemoveItem(int bookId);
-        Task<ShoppingCart> GetUserCart();
-    }
-}
--- Repositories/IHomeRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BookShopping1
-{
-    public interface IHomeRepository
-    {
-        Task<IEnumerable<Book>> GetBooks(string sTerm = "", int genreId = 0);
-        Task<IEnumerable<Genre>> Genres();
-
-    }
-}
